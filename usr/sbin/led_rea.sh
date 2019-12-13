8ef0bcef1e106d92cd0c674eca0f12a79507ea77
Rea <rea.naegeli@schulemb.ch>
2019-12-13 10:07:36 +0000

    add own led script

---
+++ usr/sbin/led_rea.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#define GPIO port
+echo "17" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio17/direction
+
+# execute something :-)
+echo "1" > /sys/class/gpio/gpio17/value 
+sleep 6 
+echo "0" > /sys/class/gpio/gpio17/value
+sleep 3
+echo "1" > /sys/class/gpio/gpio17/value
