226a178daecf95a93ee1af7e34f783a59c1fd12b
David Hügli <david.huegli@gmx.net>
2020-01-17 10:55:45 +0100

    add guess

---
+++ python/guess2.py
@@ -0,0 +1,19 @@
+import random
+randomNumber = random.randrange(0,100)
+print("Random number has been generated")
+guessed = False
+while guessed==False:
+    userInput = int(input("Your guess pleas: "))
+    if userInput==randomNumber:
+        guessed = True
+        print("Well done!")
+    elif userInput>100:
+        print("Our guess range is between 0 and 100, please try a bit lower")
+    elif userInput<0:
+        print("Our guess range is between 0 and 100, please try a bit higher")
+    elif userInput>randomNumber:
+        print("Try one more time, a bit lower")
+    elif userInput < randomNumber:
+        print("Try one more time, a bit higher")
+
+print("End of program")
